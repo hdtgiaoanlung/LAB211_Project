e2a95b09f822e7a518be626ae6f3a1c261dc6aea
Hà Đình Tâm <hadinhtam.ctf@gmail.com>
2022-09-29 22:11:23 +0700

    Update Utils

---
+++ StudentManageProject/src/utils/StringUtils.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author dinht
+ */
+public class StringUtils {
+
+    public static String removeAllBlank(String input) {
+        return input.trim().replaceAll("\\s+", "");
+    }
+
+    public static String removeUnnecessaryBlank(String input) {
+        return input.trim().replaceAll("\\s+", " ");
+    }
+
+    public static String normalFormName(String input) {
+        input = removeUnnecessaryBlank(input);
+        String temp[] = input.split(" ");
+        input = "";
+        for (int i = 0; i < temp.length; i++) {
+            input += String.valueOf(temp[i].charAt(0)).toUpperCase() + temp[i].substring(1);
+            if (i < temp.length - 1) {
+                input += " ";
+            }
+        }
+        return input;
+    }
+
+}
