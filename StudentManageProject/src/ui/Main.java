5042d0e9d6766d9f1ee19d7dbe0a4d449c665c3e
Hà Đình Tâm <101781117+hdtgiaoanlung@users.noreply.github.com>
2022-10-03 16:33:00 +0700

    UI update

--- StudentManageProject/src/ui/Main.java
+++ StudentManageProject/src/ui/Main.java
@@ -5,6 +5,9 @@
  */
 package ui;
 
+import controller.StudentController;
+import utils.StringUtils;
+
 /**
  * @author dinht
  */
@@ -14,14 +17,22 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        StudentController sc = new StudentController();
         String menu = "\nWELCOME TO STUDENT MANAGEMENT\n"
                 + "1. Create\n"
                 + "2. Find and Sort\n"
                 + "3. Update/Delete\n"
                 + "4. Report\n"
-                + "Exit\n"
+                + "5. Exit\n"
                 + "(Please choose 1 to Create, 2 to Find and Sort, 3 to Update/Delete, 4 to Report and 5 to Exit program)\n\n";
         System.out.println(menu);
+//        while (true) {
+//            int choice = StringUtils.getInt("Enter your choice: ", "Input number only!", "Input must be in range of [1, 5]", 1, 5);
+//            switch (choice) {
+//                case 1:
+//
+//            }
+//        }
 
     }
 
