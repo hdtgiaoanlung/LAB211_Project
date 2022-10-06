e526f7e2e3bb5a5e55ed2343c7a8964834cf545a
Hà Đình Tâm <101781117+hdtgiaoanlung@users.noreply.github.com>
2022-10-06 18:12:08 +0700

    Initialize bo

--- BaseConvert/src/bo/BaseConverter.java
+++ BaseConvert/src/bo/BaseConverter.java
@@ -1,4 +1,13 @@
 package bo;
 
 public class BaseConverter {
+    public BaseConverter() {
+
+    }
+
+    public String decToBin(int number) {
+        while (number != 0) {
+            
+        }
+    }
 }
