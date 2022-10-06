947d5bfd96a620ab71f59a11ae2bbe3e415f3764
Hà Đình Tâm <101781117+hdtgiaoanlung@users.noreply.github.com>
2022-10-06 18:24:28 +0700

    Update ui

--- BaseConvert/src/ui/Main.java
+++ BaseConvert/src/ui/Main.java
@@ -1,16 +1,21 @@
 package ui;
 
 import bo.BaseConverter;
+import bo.BaseInputter;
+import utils.InputUtils;
 
 public class Main {
     public static void main(String[] args) {
         BaseConverter baseConverter;
-
+        BaseInputter baseInputter;
 
         String menu = "WELCOME TO BASE CONVERT SYSTEM\n"
                 + "1. Binary\n"
                 + "2. Decimal\n"
                 + "3. Hexadecimal\n";
+        do {
+            System.out.println(menu);
 
+        } while (InputUtils.pressYNToContinue());
     }
 }
