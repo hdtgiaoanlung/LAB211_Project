a492a50f6f13e6d30b6c52869d4b343df9ace7c7
Hà Đình Tâm <hadinhtam.ctf@gmail.com>
2022-10-09 22:37:41 +0700

    .

--- BaseConvert/src/controller/BaseConvertController.java
+++ BaseConvert/src/controller/BaseConvertController.java
@@ -1,4 +1,24 @@
 package controller;
 
+import bo.BaseConverter;
+import utils.InputUtils;
+
 public class BaseConvertController {
+
+    BaseConverter baseConverter;
+    public BaseConvertController() {
+
+    }
+
+    public String convertByChoice(String number, int choice) {
+        int baseChoice = InputUtils.getInt("Enter your base to convert (1. Binary; 2. Decimal; 3. Hexadecimal)", "Input number only!", "Input must be in range of [1, 3]", 1, 3);
+        if (baseChoice == choice) {
+            return number.trim().toUpperCase();
+        } else {
+            switch (baseChoice) {
+                case 1:
+
+            }
+        }
+    }
 }
--- BaseConvert/src/ui/Main.java
+++ BaseConvert/src/ui/Main.java
@@ -7,15 +7,32 @@ import utils.InputUtils;
 
 public class Main {
     public static void main(String[] args) {
-        BaseConvertController baseConvertController;
+        BaseConvertController baseConvertController = new BaseConvertController();
 
         String menu = "WELCOME TO BASE CONVERT SYSTEM\n"
                 + "1. Binary\n"
                 + "2. Decimal\n"
-                + "3. Hexadecimal\n";
+                + "3. Hexadecimal\n"
+                + "4. Exit\n";
         do {
             System.out.println(menu);
-
+            int choice = InputUtils.getInt("Enter your choice: ", "Input numbers only!", "Input must be in range of [1, 4]", 1, 4);
+            switch (choice) {
+                case 1:
+                    String number = InputUtils.getStringByRegex("Enter your binary number: ", "Binary number contains only 0 or 1!", "[01 ]+");
+                    baseConvertController.convertByChoice(number, choice);
+                    break;
+                case 2:
+                    number = InputUtils.getStringByRegex("Enter your decimal number: ", "Enter number only!", "[0-9 ]+");
+                    baseConvertController.convertByChoice(number, choice);
+                    break;
+                case 3:
+                    number = InputUtils.getStringByRegex("Enter your hexadecimal number: ", "String is not valid!", "[0-9A-Fa-f ]+");
+                    baseConvertController.convertByChoice(number, choice);
+                    break;
+                default:
+                    return;
+            }
         } while (InputUtils.pressYNToContinue());
     }
 }
