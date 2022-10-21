285fb25aca3c179b59e2991280d4a2a270a67ea1
Hà Đình Tâm <hadinhtam.ctf@gmail.com>
2022-10-21 08:13:27 +0700

    Update Calculator

--- MatrixProject/src/bo/MatrixCalculator.java
+++ MatrixProject/src/bo/MatrixCalculator.java
@@ -7,45 +7,63 @@ public class MatrixCalculator {
     public MatrixCalculator() {
 
     }
-    public Matrix addTwoMatrix(Matrix m1, Matrix m2) throws Exception{
-        if (m1.getColumns() == m2.getColumns() && m1.getRows() == m2.getRows()) {
-            Matrix ret = new Matrix(m1.getRows(), m1.getColumns());
-            for (int i = 0; i < ret.getRows(); i++) {
-                for (int j = 0 ; j < ret.getColumns(); j++) {
-                    ret.setFactorValue(i, j, (m1.getFactorValue(i, j) + m2.getFactorValue(i, j)));
+
+    private int[][] addOrSubtract(int[][] arr1, int[][] arr2, boolean isAdding){
+        int[][] array = arr1;
+        for(int i = 0; i < arr1.length; i++) {
+            for(int j = 0; j < arr1[i].length; j++){
+                array[i][j] = isAdding ? (arr1[i][j] + arr2[i][j]) : (arr1[i][j] - arr2[i][j]);
+            }
+        }
+        return array;
+    }
+
+    private int[][] multiply(int[][] arr1, int[][] arr2) {
+        int[][] array = arr1;
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                for (int k = 0; k < arr2.length; k++) {
+                    array[i][j] += arr1[i][k] * arr2[k][j];
                 }
             }
+        }
+        return array;
+    }
+
+    public Matrix addTwoMatrix(Matrix m1, Matrix m2) throws Exception {
+        if (m1 == null || m2 == null) {
+            throw new Exception("Cannot calculate with null matrix!");
+        }
+        if (m1.getColumns() == m2.getColumns() && m1.getRows() == m2.getRows()) {
+            Matrix ret = new Matrix(m1.getRows(), m1.getColumns());
+            ret.setMatrixValue(addOrSubtract(m1.getMatrixValue(), m2.getMatrixValue(), true));
             return ret;
         }
         throw new Exception("Cannot add two matrix with two different size!");
     }
 
     public Matrix subtractTwoMatrix(Matrix m1, Matrix m2) throws Exception {
+        if (m1 == null || m2 == null) {
+            throw new Exception("Cannot calculate with null matrix!");
+        }
         if (m1.getColumns() == m2.getColumns() && m1.getRows() == m2.getRows()) {
             Matrix ret = new Matrix(m1.getRows(), m1.getRows());
-            for (int i = 0; i < m1.getRows(); i++) {
-                for (int j = 0 ; j < m1.getColumns(); j++) {
-                    ret.setFactorValue(i, j, m1.getFactorValue(i, j) - m2.getFactorValue(i, j));
-                }
-            }
+            ret.setMatrixValue(addOrSubtract(m1.getMatrixValue(), m2.getMatrixValue(), false));
             return ret;
         }
         throw new Exception("Cannot subtract two matrix with two different size!");
     }
 
     public Matrix multiplyTwoMatrix(Matrix m1, Matrix m2) throws Exception {
+        if (m1 == null || m2 == null) {
+            throw new Exception("Cannot calculate with null matrix!");
+        }
         if (m1.getColumns() == m2.getRows()) {
             Matrix ret = new Matrix(m1.getRows(), m2.getColumns());
-            for (int i = 0; i < m1.getRows(); i++) {
-                for (int j = 0; j < m2.getColumns(); j++) {
-                    ret.setFactorValue(i, j, 0);
-                    for (int k = 0; k < m2.getRows(); k++) {
-                        ret.setFactorValue(i, j, m1.getFactorValue(i, k) * m2.getFactorValue(k, j));
-                    }
-                }
-            }
+            ret.setMatrixValue(multiply(m1.getMatrixValue(), m2.getMatrixValue()));
             return ret;
         }
         throw new Exception("Cannot multiply these two matrices!");
     }
+
 }
